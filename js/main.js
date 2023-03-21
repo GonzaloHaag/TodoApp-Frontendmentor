c846ece40c24f33599bafc143d8fd38298aadc9e
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-03-21 19:39:20 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Input
+const inputTarea = document.querySelector(".input-tareaRealizar");
+
+
+inputTarea.addEventListener("change",(e)=>{
+   console.log(inputTarea.value); //Del input debo imprimir el value, no del evento
+
+   document.createElement = div;
+   
+})
