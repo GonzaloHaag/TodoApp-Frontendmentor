f4c80d5c6387165bd52b1a8cbd4d9a57fa6b1647
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-03-22 17:16:33 -0300

    Arrancamos de nuevo,trabado en el recorrido de las tareas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,86 @@
-//Input
-const inputTarea = document.querySelector(".input-tareaRealizar");
+let form = document.querySelector(".form");
+const inputTarea = document.querySelector(".inputTarea");
+const divContainer = document.querySelector(".tareas");
 
 
-inputTarea.addEventListener("change",(e)=>{
-   console.log(inputTarea.value); //Del input debo imprimir el value, no del evento
 
-   document.createElement = div;
-   
+
+
+form.addEventListener("submit",(e)=>{
+
+   let tareaArealizar = document.querySelector(inputTarea.value); //Lo que escribo en el input
+   e.preventDefault();
+   addTarea();
+   form.reset();
 })
+
+
+
+function addTarea () {
+   let div = document.createElement("div");
+   div.className = "tarea";
+
+   div.innerHTML = `
+   <div class="input-tarea">
+   <input class="check" type="checkbox">
+   <p class="tareaPorHacer">${inputTarea.value}</p>
+   </div>
+   <img class="closeTarea" src="./images/icon-cross.svg">
+    `;
+
+   divContainer.append(div);
+
+   let inputCheckbox = document.querySelectorAll(".check");
+   let tareaPorHacer = document.querySelectorAll(".tareaPorHacer");
+   /*inputCheckbox.addEventListener("click",()=>{
+
+       if(inputCheckbox.checked) { //Si esta clickeado arroja true
+         tareaPorHacer.style.textDecoration = "line-through";
+       }
+       else{
+         tareaPorHacer.style.textDecoration = "none"; //sacamos el tachado
+       }
+      
+   })*/
+   inputCheckbox.forEach((check)=>{
+      check.addEventListener("click",()=>{
+
+         if(inputCheckbox.checked) {
+            tareaPorHacer.forEach((tarea)=>{
+            
+               tarea.style.textDecoration = "line-through";
+            })
+            
+         }
+         else{
+            tareaPorHacer.forEach((tarea)=>{
+               tarea.style.textDecoration = "none";
+            })
+
+         }
+      })
+   })
+
+  
+  
+   
+
+}
+
+function agregarDivBottom () {
+
+   let divBottom = document.createElement("div");
+   divBottom.className = "itemsLeft-clear";
+
+   divBottom.innerHTML = `
+   p><span>0</span> items left</p>
+   <p>Clear completed</p>
+  `
+ divBottom.append(divContainer);
+   
+   
+      
+ 
+   
+}
+
